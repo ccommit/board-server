f92f15c1b9c05bd3448cee3d4e1667f4acfd6b4e
DeungHeok Chung <richard0326@naver.com>
2024-05-28 00:24:24 +0900

    spring io 생성
    빌드 :Gradle - kotlin
    Java 17
    Spring Boot 3.3.0

---
+++ src/test/java/richard0326/project/boarder/BoarderApplicationTests.java
@@ -0,0 +1,13 @@
+package richard0326.project.boarder;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BoarderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
